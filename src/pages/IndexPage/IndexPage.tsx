5bdf09ca86ab10a386f1ca3a6529d6b675c02fac
romanychev-l <leonid20.0831@gmail.com>
2024-03-31 11:57:25 +0300

    Add steps request

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,18 +1,30 @@
 import './IndexPage.css';
 
 import type { FC } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Link } from '../../components/Link';
 import { Page } from '../../components/Page';
 import { routes } from '../../navigation/routes.ts';
 
 export const IndexPage: FC = () => {
+  const [number, setNumber] = useState(null);
+
+  useEffect(() => {
+    fetch('https://pmpu.site/gigicoin/get_steps?tg_id=248603604')
+      .then(response => response.json())
+      .then(data => setNumber(data.steps));
+  }, []);
+
   return (
-    <Page title="Home Page">
-      <p>
+    <Page title="Home">
+      {/* <p>
         This page is a home page in this boilerplate. You can use the links below to visit other
         pages with their own functionality.
-      </p>
+      </p> */}
+      <div>
+        {number && <h1>{number}</h1>}
+      </div>
       <ul className="index-page__links">
         {routes.map(({ path, title }) => title && (
           <li className="index-page__link" key={path}>
